ad3e067f7af70bff23f6369006d7f95214636f24
annylee0723 <annylee0723@gmail.com>
2024-09-26 15:49:05 +0900

    Feat: #4-create Route and connected to server

---
+++ routes/countRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+import { getCountList } from "../controllers/countController.js";
+
+const router = express.Router();
+
+router.get("/company/counts", getCountList);
+
+export default router;
