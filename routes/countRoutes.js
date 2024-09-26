904f7c0e1608f39bfb751e867c6c06a8c8f7ba76
annylee0723 <annylee0723@gmail.com>
2024-09-26 17:58:30 +0900

    Feat: #7 add Endpoint, add PUT api, add routes

--- routes/countRoutes.js
+++ routes/countRoutes.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { getCountList } from "../controllers/countController.js";
+import {
+  getCountList,
+  putMyCount,
+  putComparedCount,
+} from "../controllers/countController.js";
 
 const router = express.Router();
 
 router.get("/companies/counts", getCountList);
+router.put("/companies/:id/my-chosen-count", putMyCount);
+router.put("/companies/:id/compared-chosen-count", putComparedCount);
 
 export default router;
