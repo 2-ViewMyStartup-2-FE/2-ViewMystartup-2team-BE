f9c6149087dbe74fb98475710d0b546ec71e9aa1
annylee0723 <annylee0723@gmail.com>
2024-09-27 13:29:26 +0900

    Feat: #11-Set CORS

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import countRoutes from "./routes/countRoutes.js";
+import cors from "cors";
 
 const app = express();
+const corsOptions = {
+  origin: ["http://127.0.0.1:3000"], //호스트 번호 수정 가능, 나중에 FE 주소 추가 필요
+};
+
 app.use(express.json());
+app.use(cors(corsOptions));
 
 app.use("/api", countRoutes);
 
