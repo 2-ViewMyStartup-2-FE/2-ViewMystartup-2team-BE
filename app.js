9cf29c5c9e83eacd872a2fec4b5d951da6c4775f
annylee0723 <annylee0723@gmail.com>
2024-10-01 14:15:59 +0900

    fetch: develop branch merge

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ import cors from "cors";
 
 const app = express();
 const corsOptions = {
-  origin: ["http://127.0.0.1:3000"], //호스트 번호 수정 가능, 나중에 FE 주소 추가 필요
+  origin: ["http://localhost:3000/", "http://localhost:3000/compare-status"], //호스트 번호 수정 가능, 나중에 FE 주소 추가 필요
 };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use("/api", countRoutes);
 
-app.listen(process.env.PORT || 3000, () => console.log("server started"));
+app.listen(process.env.PORT || 4000, () => console.log("server started"));
