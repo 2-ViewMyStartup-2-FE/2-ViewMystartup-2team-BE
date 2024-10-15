9dc3794367657e18d48b91b80b3214674d2b99cc
TaeyeonKim <gggm0117@naver.com>
2024-10-15 17:49:20 +0900

    delete

--- routes/investmentRoutes.js
+++ routes/investmentRoutes.js
@@ -1,14 +1,12 @@
 import express from "express";
 import {
   getInvestmentList,
-  getInvestment,
-  postInvestment,
+  postInvestment
 } from "../controllers/investmentController.js";
 
 const router = express.Router();
 
 router.get("/investments", getInvestmentList);
-router.get("/investments/:id", getInvestment);
 router.post("/companies/:id/investments", postInvestment);
 
 export default router;
